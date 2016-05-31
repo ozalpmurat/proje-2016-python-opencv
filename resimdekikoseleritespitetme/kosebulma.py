817a522f43e5e1cbc2a6fe5c4d383b08a4c92018
ishak <ishakyavuzer@users.noreply.github.com>
2016-05-31 16:31:04 +0300

    Add files via upload

---
+++ resimdekikoseleritespitetme/kosebulma.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+
+filename = 'damatahtasi.jpg'
+img = cv2.imread(filename)
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+gray = np.float32(gray)
+dst = cv2.cornerHarris(gray,2,3,0.04)
+
+dst = cv2.dilate(dst,None)
+
+
+img[dst>0.01*dst.max()]=[0,0,255]
+
+cv2.imshow('resim',img)
+if cv2.waitKey(0) & 0xff == 27:
+    cv2.destroyAllWindows()
